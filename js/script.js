cb71c0cc948e27bd736940bfda4ee77109c0a5b6
Aleksander <guree.19.05.94@gmail.com>
2023-01-08 13:39:23 +0200

    1

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+ $('.slider-porfolio').slick({
+    arrow:true,
+    dots:false,
+    adaptiveHeight:true,
+    slidesToShow:3,
+    slidesToScroll:3,
+    speed:500,
+    easing:'easeInQuint',
+    infinite:false,
+    adaptiveHeight:true,
+    appendArrows:$('.page-portfolio_buttons'),
+ });
+}); 
+$(document).ready(function(){
+    $('.reviews__slider').slick({
+        arrow:true,
+        dots:false,
+        adaptiveHeight:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed:500,
+        easing:'easeInQuint',
+        infinite:false,
+        adaptiveHeight:true,
+        appendArrows:$('.content__reviews-btns'),
+    });
+   }); 
